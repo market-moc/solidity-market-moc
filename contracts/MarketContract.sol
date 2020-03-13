b6f902328f75432b89b44fcbf1b04c2a2f8230cc
tibdev78 <thibaut.desaintvenant@hotmail.com>
2020-03-13 17:59:18 +0100

    final alsmot done v2

--- contracts/MarketContract.sol
+++ contracts/MarketContract.sol
@@ -1,9 +1,6 @@
 pragma solidity >=0.4.21 <0.7.0;
 pragma experimental ABIEncoderV2;
 
-// import "../models/house.sol";
-// import "../models/person.sol";
-
 contract MarketContract {
     struct House {
         string tp;
